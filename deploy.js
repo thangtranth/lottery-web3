53f90649a420546d1d1fb0b2dd538f7b02c41a7f
Thang Tran <thangtran@brodev.com>
2022-07-05 17:48:23 +0700

    first commit

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "staff endorse canvas wage month silver sheriff surround silver tower shop offer",
+  "https://rinkeby.infura.io/v3/d288897db2f64d18a7ec1a2ea636b163"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
